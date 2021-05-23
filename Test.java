7eb8f10dbc6478e184ca5c24bd2c246d8e3de2c2
Shalini199 <84658661+Shalini199@users.noreply.github.com>
2021-05-23 13:32:53 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,27 @@
+
+ class est  
+{
+private est()
+{
+System.out.println("Ddcd");
+}
+}
+class Test extends est
+{
+System.out.println("SSS");
+public static void main (String args[])
+{
+Test t = new Test();
+}
+}
+
+
+
+
+
+
+
+
+    
+
+
